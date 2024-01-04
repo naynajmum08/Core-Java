1b71d31df7034d1995f4e7e21941e9ee9cc80a85
Hp <Hp@DESKTOP-MEHK842>
2024-01-04 13:20:53 +0530

    Day 1 programs

---
+++ DayOne/src/FirstProgram.java
@@ -0,0 +1,10 @@
+//to demonstrate simple java program
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome students to the TNSIF");
+		
+
+	}
+
+}
