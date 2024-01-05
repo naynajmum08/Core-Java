c43302ee67e392b46f6087a190f48fe14a16116c
Hp <Hp@DESKTOP-MEHK842>
2024-01-05 15:05:28 +0530

    Encapsulation

---
+++ DayThree/src/com/tnsif/encapsulationdemo/OopsDemo.java
@@ -0,0 +1,17 @@
+package com.tnsif.encapsulationdemo;
+
+public class OopsDemo {
+
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+
+		obj.setSerialNum(10);
+		obj.setName("Brijesh");
+		obj.setAge(23);
+		System.out.println(obj.getAge());
+
+		// obj.name="nayna";
+		System.out.println(obj);
+	}
+
+}
